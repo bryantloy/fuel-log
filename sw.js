043b464b38152ca92556cf6fdb67c4bd77977743
Bryant Loy <bryant.r.loy@gmail.com>
2026-09-13 16:35:49 -0500

    Fixed

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Network-first so app updates always land. Cache is only an offline fallback.
-const CACHE = 'fuellog-v13';
+const CACHE = 'fuellog-v15';
 const SHELL = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
